8dd3dccbba707476d778b3d9076d978d7097c18a
FaruqKhan11148 <faruqkhanshired111@gmail.com>
2026-09-15 00:20:59 +0530

    UI Changes

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import './Home.css';
 
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useLanguage } from '../../context/LanguageContext';
 
