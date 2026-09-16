a44561f1bf22c2c48a2aa9ac93551870a724a612
FaruqKhan11148 <faruqkhanshired111@gmail.com>
2026-09-16 16:54:35 +0530

    updates in shop ui

--- frontend/src/pages/Shop/Shop.jsx
+++ frontend/src/pages/Shop/Shop.jsx
@@ -19,7 +19,6 @@ function Shop() {
   const [flashMessage, setFlashMessage] = useState('');
 
   const [selectedCategory, setSelectedCategory] = useState('All');
-  const [orderType, setOrderType] = useState('delivery');
 
   const COMMON_SHOP_IMAGE =
     'https://res.cloudinary.com/dsznfqgu3/image/upload/v1789550262/Gemini_Generated_Image_2u7tlf2u7tlf2u7t.png';
